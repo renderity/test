289da695f870ab170828ee5ba69dd307389313f4
dsbelov63 <dsbelov63@gmail.com>
2022-03-09 17:46:14 +0400

    saved current state

--- src/main.cpp
+++ src/main.cpp
@@ -553,96 +553,12 @@ extern "C" void constructRenderityWrappers (void)
 
 
 
-				// // SPIR-V has no built-in inverse ?
-				// mat4 _inverse (mat4 m)
-				// {
-				// 	float
-				// 			a00 = m[0][0], a01 = m[0][1], a02 = m[0][2], a03 = m[0][3],
-				// 			a10 = m[1][0], a11 = m[1][1], a12 = m[1][2], a13 = m[1][3],
-				// 			a20 = m[2][0], a21 = m[2][1], a22 = m[2][2], a23 = m[2][3],
-				// 			a30 = m[3][0], a31 = m[3][1], a32 = m[3][2], a33 = m[3][3],
-
-				// 			b00 = a00 * a11 - a01 * a10,
-				// 			b01 = a00 * a12 - a02 * a10,
-				// 			b02 = a00 * a13 - a03 * a10,
-				// 			b03 = a01 * a12 - a02 * a11,
-				// 			b04 = a01 * a13 - a03 * a11,
-				// 			b05 = a02 * a13 - a03 * a12,
-				// 			b06 = a20 * a31 - a21 * a30,
-				// 			b07 = a20 * a32 - a22 * a30,
-				// 			b08 = a20 * a33 - a23 * a30,
-				// 			b09 = a21 * a32 - a22 * a31,
-				// 			b10 = a21 * a33 - a23 * a31,
-				// 			b11 = a22 * a33 - a23 * a32,
-
-				// 			det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
-
-				// 	return mat4(
-				// 			a11 * b11 - a12 * b10 + a13 * b09,
-				// 			a02 * b10 - a01 * b11 - a03 * b09,
-				// 			a31 * b05 - a32 * b04 + a33 * b03,
-				// 			a22 * b04 - a21 * b05 - a23 * b03,
-				// 			a12 * b08 - a10 * b11 - a13 * b07,
-				// 			a00 * b11 - a02 * b08 + a03 * b07,
-				// 			a32 * b02 - a30 * b05 - a33 * b01,
-				// 			a20 * b05 - a22 * b02 + a23 * b01,
-				// 			a10 * b10 - a11 * b08 + a13 * b06,
-				// 			a01 * b08 - a00 * b10 - a03 * b06,
-				// 			a30 * b04 - a31 * b02 + a33 * b00,
-				// 			a21 * b02 - a20 * b04 - a23 * b00,
-				// 			a11 * b07 - a10 * b09 - a12 * b06,
-				// 			a00 * b09 - a01 * b07 + a02 * b06,
-				// 			a31 * b01 - a30 * b03 - a32 * b00,
-				// 			a20 * b03 - a21 * b01 + a22 * b00) / det;
-				// }
-
-
-
-				// Ray getRayFromPixel (vec2 pixel_coord)
-				// {
-				// 	Ray ray;
-
-				// 	ray.origin = camera.matrix[3].xyz;
-
-				// 	ray.direction.xy = pixel_coord;
-				// 	ray.direction.z = 0.5f;
-
-				// 	// ray.direction = (camera.matrix * _inverse(camera.projection_matrix) * vec4(ray.direction, 1.0)).xyz;
-				// 	ray.direction = (_inverse(camera.view_matrix) * _inverse(camera.projection_matrix) * vec4(ray.direction, 1.0)).xyz;
-				// 	ray.direction = normalize(ray.direction - ray.origin);
-
-				// 	return ray;
-				// }
-
-
-
 				vec3 intersection_box = ray.origin;
 				vec3 intersection_box_far = ray.origin;
 				vec3 intersection_box_far1 = ray.origin;
 				vec3 intersection = ray.origin;
 
 
-
-				// bool getIntersectionRayBox (Ray ray, vec3 box_min, vec3 box_max)
-				// {
-				// 	vec3 tMin = (box_min - ray.origin) / ray.direction;
-				// 	vec3 tMax = (box_max - ray.origin) / ray.direction;
-				// 	vec3 t1 = min(tMin, tMax);
-				// 	vec3 t2 = max(tMin, tMax);
-				// 	float tNear = max(max(t1.x, t1.y), t1.z);
-				// 	float tFar = min(min(t2.x, t2.y), t2.z);
-
-				// 	if (tNear > tFar)
-				// 	{
-				// 		return false;
-				// 	}
-
-				// 	intersection_box = ray.direction;
-				// 	intersection_box *= tNear;
-				// 	intersection_box += ray.origin;
-
-				// 	return true;
-				// }
 
 				bool getIntersectionRayBox (Ray ray, vec3 box_min, vec3 box_max)
 				{
@@ -740,8 +656,10 @@ extern "C" void constructRenderityWrappers (void)
 					vec3 p1 = vec3(0.0f);
 					vec3 p2 = vec3(0.0f);
 					vec3 p3 = vec3(0.0f);
-					vec3 min = vec3(-10.0f, -10.0f, -10.0f);
-					vec3 max = vec3(10.0f, 10.0f, 10.0f);
+					// vec3 min = vec3(-10.0f, -10.0f, -10.0f);
+					// vec3 max = vec3(10.0f, 10.0f, 10.0f);
+					vec3 min = vec3(-13.0f, -13.0f, -13.0f);
+					vec3 max = vec3(13.0f, 13.0f, 13.0f);
 
 					float nearest_ray_triangle_intersection = 999999.0f;
 
@@ -758,6 +676,7 @@ extern "C" void constructRenderityWrappers (void)
 
 						ivec3 xyz = clamp(ivec3((intersection_box + (size * 0.5f)) / segment_size), ivec3(0), ivec3(dimension_segment_count - 1));
 
+						// TODO: use custom udot function?
 						uint box_index = uint(dot(xyz, dimensions));
 
 						for (uint i = 0; i < dimension_segment_count; ++i)
@@ -820,10 +739,15 @@ extern "C" void constructRenderityWrappers (void)
 
 									float ray_origin_to_intersection_distance = distance(ray.origin, intersection);
 
-									if (ray_origin_to_intersection_distance < nearest_ray_triangle_intersection && ray_origin_to_intersection_distance > 0.001f)
-									{
-										nearest_ray_triangle_intersection = ray_origin_to_intersection_distance;
-									}
+									// if (ray_origin_to_intersection_distance < nearest_ray_triangle_intersection)
+									// {
+									// 	nearest_ray_triangle_intersection = ray_origin_to_intersection_distance;
+									// }
+
+									nearest_ray_triangle_intersection =
+										(1.0f - step(nearest_ray_triangle_intersection, ray_origin_to_intersection_distance)) * distance(ray.origin, intersection);
+
+									break;
 								}
 							}
 
